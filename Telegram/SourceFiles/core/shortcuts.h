09afc52b12ff6d14f57250d74e01289530a033d8
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 03:40:43 +0300

    [Improvement] Shortcuts to switch accounts

--- Telegram/SourceFiles/core/shortcuts.h
+++ Telegram/SourceFiles/core/shortcuts.h
@@ -47,6 +47,10 @@ enum class Command {
 	ShowAccount4,
 	ShowAccount5,
 	ShowAccount6,
+	ShowAccount7,
+	ShowAccount8,
+	ShowAccount9,
+	ShowAccountLast,
 
 	ShowAllChats,
 	ShowFolder1,
@@ -114,6 +118,10 @@ enum class Command {
 	Command::ShowAccount4,
 	Command::ShowAccount5,
 	Command::ShowAccount6,
+	Command::ShowAccount7,
+	Command::ShowAccount8,
+	Command::ShowAccount9,
+	Command::ShowAccountLast,
 };
 
 [[nodiscard]] FnMut<bool()> RequestHandler(Command command);
